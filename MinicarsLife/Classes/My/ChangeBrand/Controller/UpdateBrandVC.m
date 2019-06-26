8aec06684bba431d7bc1a1be6ecb73594506ff74
杰 <792574770@qq.com>
2019-06-26 17:22:18 +0800

    no message

---
+++ MinicarsLife/Classes/My/ChangeBrand/Controller/UpdateBrandVC.m
@@ -0,0 +1,21 @@
+//
+//  UpdateBrandVC.m
+//  MinicarsLife
+//
+//  Created by 梅敏杰 on 2019/6/26.
+//  Copyright © 2019 QinBao Zheng. All rights reserved.
+//
+
+#import "UpdateBrandVC.h"
+
+@implementation UpdateBrandVC
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
